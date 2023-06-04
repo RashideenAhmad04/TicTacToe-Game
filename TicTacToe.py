3e38b436c10c17ebc8270919836e48d00a8cf37e
RashideenAhmad04 <10012022rb@gmail.com>
2023-06-04 10:59:45 +0800

    Loading Screen adn Lobby

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,123 +1,32 @@
 import pygame
-import random
+from sys import exit
+import os
 
 # Initialize Pygame
 pygame.init()
 
 # Set up the game window
-width, height = 1920, 1080
+width, height = 1920, 900
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Up You Go! Xs and Os - Tic Tac Toe")
+clock = pygame.time.Clock()
 
-# Colors
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+# Loading Screen
+Loading_Screen = pygame.image.load("F:\Rashideen\CodingProjects\Rania\Starting_Screen.png").convert()
+Loading_Screen = pygame.transform.scale(Loading_Screen, (width, height))
+screen.blit(Loading_Screen, (0, 0))
 
-# Game variables
-grid_size = 5
-cell_size = width // grid_size
-wall_prob = 0.2
-
-# Player variables
-player1_turn = True
-player1_symbol = "X"
-player2_symbol = "O"
-
-# Create the game grid
-grid = [["" for _ in range(grid_size)] for _ in range(grid_size)]
-
-# Create random walls
-walls = []
-for i in range(grid_size - 1):
-    for j in range(grid_size - 1):
-        if random.random() < wall_prob:
-            walls.append((i, j))
-
-# Main menu loop
-
-
-def main_menu():
-    screen.fill(WHITE)
-    pygame.display.flip()
-
-    waiting = True
-    while waiting:
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                waiting = False
-
-# Lobby loop
-
-
-def lobby():
-    screen.fill(WHITE)
-    font = pygame.font.Font(None, 36)
-    text = font.render("Press SPACE to be ready", True, BLACK)
-    text_rect = text.get_rect(center=(width // 2, height // 2))
-    screen.blit(text, text_rect)
-    pygame.display.flip()
-
-    player1_ready = False
-    player2_ready = False
-    waiting = True
-    while waiting:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    if player1_ready and player2_ready:
-                        waiting = False
-                    elif player1_turn:
-                        player1_ready = True
-                        player1_turn = False
-                    else:
-                        player2_ready = True
-                        player1_turn = True
-
-# Game loop
-
-
-def game():
-    running = True
-    clock = pygame.time.Clock()
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN and not player1_turn:
-                pos = pygame.mouse.get_pos()
-                col = pos[0] // cell_size
-                row = pos[1] // cell_size
-                if grid[row][col] == "":
-                    grid[row][col] = player2_symbol
-                    player1_turn = True
-
-        # Update screen
-        screen.fill(WHITE)
-
-        # Draw grid
-        for i in range(grid_size):
-            for j in range(grid_size):
-                rect = pygame.Rect(j * cell_size, i *
-                                   cell_size, cell_size, cell_size)
-                pygame.draw.rect(screen, BLUE, rect, 1)
-                if grid[i][j] != "":
-                    font = pygame.font.Font(None, 72)
-                    text = font.render(grid[i][j], True, BLACK)
-                    text_rect = text.get_rect(center=rect.center)
-                    screen.blit(text, text_rect)
-
-        pygame.display.flip()
-        clock.tick(60)
-
-
-# Main game loop
 while True:
-    main_menu()
-    lobby()
-    game()
-
-# Quit the game
-pygame.quit()
+    # player inputs
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            # Lobby Screen
+            Lobby_Screen = pygame.image.load('Lobby.png')
+            Lobby_Screen = pygame.transform.scale(Lobby_Screen, (width, height))
+            screen.blit(Lobby_Screen, (0, 0))
+            pygame.display.flip()
+    
+    pygame.display.update()
